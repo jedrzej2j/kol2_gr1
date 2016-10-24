0ed8358130168822d249705df33fd84b70bf5fc7
Piotr  Jedrzejowski <3jedrzejowski@stud103>
2016-10-24 18:40:46 +0200

    1

--- kol2_gr1.py
+++ kol2_gr1.py
@@ -14,3 +14,26 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+
+
+
+class Class(object):
+	def __init__(self):
+		self.students={}
+	def add_person(self,name,surname):
+		self.students[(name,surname)] = [0,0]
+	def add_score(self,name,surname,score):
+		self.students[(name,surname)][0]+=float(score)
+		self.students[(name,surname)][1]+=float(1)
+	def print_average(self,name,surname):
+		return str(self.students[(name,surname)][0]/self.students[(name,surname)][1])
+if __name__=="__main__":
+	test=Class()
+	test.add_person("a","b")
+	test.add_score("a","b",3)
+	test.add_score("a","b",5)
+	print test.print_average("a","b")
+
+
+
