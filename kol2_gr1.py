b764a64c65754dad8633d9ba1e4efc1e5bb1c902
Piotr  Jedrzejowski <3jedrzejowski@taurus.fis.agh.edu.pl>
2016-11-12 18:41:58 +0100

    Wprowadzono poprawki

--- kol2_gr1.py
+++ kol2_gr1.py
@@ -1,74 +1,102 @@
+import json
+import os
+    
 class School(object):
-  def __init__(self):
-    self.classes={}
-  def add_class(self,class_name):
-    self.classes[class_name]=Class(class_name)
+  
+    def __init__(self):
+        self.classes={}
+      
+    def add_class(self,class_name):
+        self.classes[class_name]=Class(class_name)
+  
+    def load_from_file(self,filename):
+        if os.path.isfile(filename):
+            with open(filename) as file:
+                data=json.load(file)
+            self.parse_data(data)
+                
+    def save_to_file(self,filename):
+        data={}
+        for key in self.classes:
+            data[key]=self.classes[key].students
+        with open(filename, 'w') as outfile:
+            json.dump(data, outfile) 
+              
+    def parse_data(self,data):
+        for key in data:
+            self.add_class(key)
+            self.classes[key].students=data[key] 
+            
 
 class Class(object):
-  def __init__(self,name):
-    self.students={}
-    self.name=name
-    self.subjects=[]
-  def add_person(self,name,surname):
-    self.students[(name,surname)] = {}
-  def add_score(self,name,surname,score,subject):
-    if not subject in self.students[(name,surname)].keys():
-      self.students[(name,surname)][subject]=[float(score),float(1)] 
-    else:
-      self.students[(name,surname)][subject][0]+=float(score)
-      self.students[(name,surname)][subject][1]+=float(1)
-  def add_subject(self,subject_name):
-    if subject_name not in self.subjects:
-      self.subjects.append(subject_name)
-  def print_average_subject(self,name,surname,subject):
-    return str(self.students[(name,surname)][subject][0]/self.students[(name,surname)][subject][1])
-  def print_full_class_average_subject(self,subject):
-    full_sum=0
-    full_number=0
-    for i,j in self.students:
-      full_sum+=self.students[i,j][subject][0]
-      full_number+=self.students[i,j][subject][1]
-    return full_sum/full_number
-  def print_average(self,name,surname):
-    full_sum=0
-    full_number=0
-    for s in self.students[name,surname]:
-      full_sum+=self.students[name,surname][s][0]
-      full_number+=self.students[name,surname][s][1]
-    return full_sum/full_number
-  def print_full_class_average(self):
-    full_sum=0
-    full_number=0
-    for i,j in self.students:
-      for s in self.students[i,j]:
-        full_sum+=self.students[i,j][s][0]
-        full_number+=self.students[i,j][s][1]
-    return full_sum/full_number
+
+    def __init__(self,name):
+        self.students={}
+        self.name=name
+      
+    def add_person(self,name,surname):
+       self.students[name+" "+surname] = {}
+      
+    def add_score(self,name,surname,score,subject):
+        if not subject in self.students[name+" "+surname].keys():
+            self.students[name+" "+surname][subject]=[score] 
+        else:
+            self.students[name+" "+surname][subject].append(score)
+
+
+class Print_content(object):
+
+    @staticmethod
+    def print_average_subject(class_name,name,surname,subject):
+        return str(sum(class_name.students[name+" "+surname][subject])/float(len(class_name.students[name+" "+surname][subject])))
+    
+    @staticmethod  
+    def print_full_class_average_subject(class_name,subject):
+        list_of_grades=[]
+        for i in class_name.students:
+            list_of_grades.extend(class_name.students[i][subject])
+        return sum(list_of_grades)/float(len(list_of_grades))
+    
+    @staticmethod  
+    def print_average_of_student(class_name,name,surname):
+        list_of_grades=[]
+        for s in class_name.students[name+" "+surname]:
+            list_of_grades.extend(class_name.students[name+" "+surname][s])
+        return sum(list_of_grades)/float(len(list_of_grades))
+    
+    @staticmethod  
+    def print_full_class_average(class_name):
+        list_of_grades=[]
+        for i in class_name.students:
+            for s in class_name.students[i]:
+                list_of_grades.extend(class_name.students[i][s])
+        return sum(list_of_grades)/float(len(list_of_grades))
+              
       
 if __name__=="__main__":
   
-  szkola=School()
-  szkola.add_class("1f")
-
-  szkola.classes["1f"].add_person("Janusz","Rusek")
-  szkola.classes["1f"].add_score("Janusz","Rusek",3,"biologia")
-  szkola.classes["1f"].add_score("Janusz","Rusek",1,"biologia")
-  szkola.classes["1f"].add_score("Janusz","Rusek",3,"chemia")
-  print "srednia Janusz Rusek z biologii "+szkola.classes["1f"].print_average_subject("Janusz","Rusek","biologia")
-  print "srednia Janusz Rusek z chemii "+szkola.classes["1f"].print_average_subject("Janusz","Rusek","chemia")
-
-  szkola.classes["1f"].add_person("Ben","Szybki")
-  szkola.classes["1f"].add_score("Ben","Szybki",1,"biologia")
-  szkola.classes["1f"].add_score("Ben","Szybki",1,"chemia")
-  szkola.classes["1f"].add_score("Ben","Szybki",3,"chemia")
-  szkola.classes["1f"].add_score("Ben","Szybki",5,"chemia")
-  print "srednia Ben Szybki z biologii "+szkola.classes["1f"].print_average_subject("Ben","Szybki","biologia")
-  print "srednia Ben Szybki z chemii "+szkola.classes["1f"].print_average_subject("Ben","Szybki","chemia")
+    szkola=School()
+    szkola.load_from_file("data.json")
+    szkola.classes["1f"].add_person("Stefan","Wykalaczka")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",1,"biologia")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",1,"biologia")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",6,"chemia")  
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",3,"biologia")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",1,"chemia")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",5,"chemia")
+    szkola.classes["1f"].add_score("Stefan","Wykalaczka",6,"chemia")
+    
+    print "srednia Janusz Rusek z biologii "+Print_content.print_average_subject(szkola.classes["1f"],"Janusz","Rusek","biologia")
+    print "srednia Janusz Rusek z chemii "+Print_content.print_average_subject(szkola.classes["1f"],"Janusz","Rusek","chemia")
+    print "srednia Ben Szybki z biologii "+Print_content.print_average_subject(szkola.classes["1f"],"Ben","Szybki","biologia")
+    print "srednia Ben Szybki z chemii "+Print_content.print_average_subject(szkola.classes["1f"],"Ben","Szybki","chemia")
+    print "srednia Stefan Wykalaczka z biologii "+Print_content.print_average_subject(szkola.classes["1f"],"Stefan","Wykalaczka","biologia")
+    print "srednia Stefan Wykalaczka z chemii "+Print_content.print_average_subject(szkola.classes["1f"],"Stefan","Wykalaczka","chemia")
+    print "srednia klasy z biologii "+str(Print_content.print_full_class_average_subject(szkola.classes["1f"],'biologia'))
+    print "srednia Ben Szybki "+str(Print_content.print_average_of_student(szkola.classes["1f"],"Ben","Szybki"))
+    print "srednia klasy ze wszystkich przedmiotow "+str(Print_content.print_full_class_average(szkola.classes["1f"]))
   
-  print "srednia klasy z biologii "+str(szkola.classes["1f"].print_full_class_average_subject('biologia'))
-  print "srednia Ben Szybki "+str(szkola.classes["1f"].print_average("Ben","Szybki"))
-  print "srednia klasy ze wszystkich przedmiotow "+str(szkola.classes["1f"].print_full_class_average())
-
+    #szkola.save_to_file("data.json")
 
 
 
